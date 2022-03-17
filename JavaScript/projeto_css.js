e9fd4c755fb884a49fdcd580a5e4601eafedd85e
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-03-17 10:30:34 -0300

    Initial commit

---
+++ JavaScript/projeto_css.js
@@ -0,0 +1,22 @@
+var nrImagem = 0;
+var imagens = [];
+var refrescar = 1; // mudar imagem de 1 em 1 segundo
+
+// colocar aqui todas as imagens, seguindo a ordem numérica
+imagens[0] = "imagens/moto-eyes.jpg";
+imagens[1] = "imagens/moto-eyes1.jpeg";
+imagens[2] = "imagens/moto-eyes2.jpeg";
+imagens[3] = "imagens/moto-eyes3.jpeg";
+//...erro no link das imagens
+
+function rodarImagens() {
+    //  document.getElementById('misto').src = imagens[]; //erro aqui
+   document.images["misto"].src = imagens[nrImagem++];
+
+   if (nrImagem >= imagens.length)
+      nrImagem = 0;
+
+   setTimeout("rodarImagens()", refrescar * 2800);  
+}
+
+rodarImagens();//não consigo chamar essa função
